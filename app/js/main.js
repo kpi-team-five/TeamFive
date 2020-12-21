2b2d43fe3601995de5dd1883565232dc3175ebc7
Nikita Lazarenko <37745544+UniversalCorn@users.noreply.github.com>
2020-12-22 00:03:12 +0200

    front-end logic added to authorization window

--- app/js/main.js
+++ app/js/main.js
@@ -22,3 +22,40 @@ for (let i = 0; i < items.length; i++) {
     paras[i].style.animation = "paragraph-hide 0.5s forwards"
   })
 }
+
+const nav = document.getElementsByTagName('nav')[0];
+const authorizeButton = nav.getElementsByTagName('li')[4];
+const outerForm = document.getElementsByClassName('outer')[0];
+const closeButton = document.getElementsByClassName('close-button')[0];
+const blockBackground = document.getElementsByClassName('block-background')[0];
+blockBackground.style.height = document.body.offsetHeight + 'px';
+
+authorizeButton.addEventListener('click', () => {
+  blockBackground.style.display = 'block';
+  outerForm.style.display = 'flex';
+});
+
+closeButton.addEventListener('click', () => {
+  blockBackground.style.display = 'none';
+  outerForm.style.display = 'none';
+});
+
+const tabs = document.getElementsByClassName('tab');
+const vhodTab = tabs[0];
+const regTab = tabs[1];
+const vhodForm = document.getElementById('vhod-form');
+const regForm = document.getElementById('reg-form');
+
+function tabClickListener(tab, secondTab, form, secondForm) {
+  tab.addEventListener('click', () => {
+    if (secondTab.classList.contains('active-tab')) {
+      secondTab.classList.remove('active-tab');
+      tab.classList.add('active-tab');
+      form.style.display = 'none';
+      secondForm.style.display = 'flex';
+    }
+  })
+}
+
+tabClickListener(vhodTab, regTab, regForm, vhodForm);
+tabClickListener(regTab, vhodTab, vhodForm, regForm);
