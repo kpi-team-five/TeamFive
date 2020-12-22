8c99a0710a9cbda033246ef7779632d9e785c2d3
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2020-12-22 13:40:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 const express = require('express');
+const sqlite = require('sqlite')
+const bodyParser = require('body-parser');
 
 const app = express();
 const static_root = "app";
@@ -10,3 +12,24 @@ app.use(express.static(static_root));
 app.listen(port, () => {
   console.log('Http server started');
 });
+
+//подключаем модуль sqlite3
+var sqlite3 = require('sqlite3').verbose();
+
+//создаем базу данных
+var db = new sqlite3.Database('users.db');
+
+db.serialize(function() {
+  //создаем таблицу
+  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, login TEXT, password INTEGER)")
+
+  var stmt = db.prepare("INSERT INTO users (login, password) VALUES ('example@gmail.com', '1234567890')");
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM users", function (err, row) {
+    console.log(row.id + ": " + row.info);
+  });
+});
+
+//закрывае подключение к БД
+db.close();
