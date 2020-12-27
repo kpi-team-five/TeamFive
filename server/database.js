ae406e8072d1af04b8c4a12f20aa0189cac6d8aa
UniversalCorn <lazarenkonikita1993@gmail.com>
2020-12-27 22:02:11 +0200

    structure update

--- server/database.js
+++ server/database.js
@@ -1,15 +1,11 @@
 'use strict';
 
 const { readFile } = require('fs').promises;
+const { promisify } = require('util');
 
 const sqlite3 = require('sqlite3').verbose();
 
-const database = new sqlite3.Database('./server/database/database.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the SQLite database.');
-});
+let database;
 
 const files = {
   tables: './server/database/tables.sql',
@@ -20,16 +16,23 @@ const options = {
   encoding: 'utf8',
 };
 
+const connect = callback => {
+  database = new sqlite3.Database('./server/database/teamfive.db', callback);
+};
+
+const close = callback => {
+  database.close(callback);
+};
+
+const getConnection = () => database;
+
 const init = async () => {
   console.log('Database initialization started...');
   const onResult = (res, err) => console.log(res, err);
   const tablesInput = await readFile(files.tables, options);
-  const tables = tablesInput
-    .replace(/(\r\n|\n|\r)/gm, '')
-    .split(';')
-    .filter(entry => entry.length > 0);
+  const tables = tablesInput.replace(/(\r\n|\n|\r)/gm, '').split(';').filter(entry => entry.length > 0);
   for (const table of tables) {
-    database.run(table, onResult);
+    console.log(await promisify(database.run.bind(database))(table));
   }
   console.log('Database initialization finished...');
 };
@@ -38,18 +41,17 @@ const fill = async () => {
   console.log('Database filling started...');
   const onResult = (res, err) => console.log(res, err);
   const dataInput = await readFile(files.data, options);
-  const data = dataInput
-    .replace(/(\r\n|\n|\r)/gm, '')
-    .split(';')
-    .filter(entry => entry.length > 0);
+  const data = dataInput.replace(/(\r\n|\n|\r)/gm, '').split(';').filter(entry => entry.length > 0);
   for (const entry of data) {
-    database.run(entry, onResult);
+    console.log(await promisify(database.run.bind(database))(entry));
   }
   console.log('Database filling finished...');
 };
 
 module.exports = {
-  database,
+  connect,
+  close,
+  getConnection,
   init,
   fill,
 };
--- server/main.js
+++ server/main.js
@@ -6,12 +6,23 @@ const database = require('./database');
 const app = express();
 const port = process.env.PORT || 80;
 
-const start = async () => {
+const start = () => {
+  database.connect(async () => {
+    await database.init();
+    await database.fill();
+    
+    const queries = require('./queries');
+    console.log(await queries.UserQueries.add({ login: 'login3', pass: 'pass3' }));
+    console.log(await queries.UserQueries.getAll());
+    console.log(await queries.UserQueries.update(3, { login: 'loginNot3', pass: 'passNot3'}));
+    console.log(await queries.UserQueries.getOne(3));
+    console.log(await queries.UserQueries.delete(3));
+    console.log(await queries.UserQueries.getAll());
+    
+  });
   app.listen(port, () => {
     console.log('Http server started');
   });
-  await database.init();
-  await database.fill();
 };
 
 module.exports = {
